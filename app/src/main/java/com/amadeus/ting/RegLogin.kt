2ce8a50b5cdd5338c7d70ae975ea60474a718015
Johannes <zberzerker246@gmail.com>
2023-03-06 03:46:49 +0800

    Added a few assets and placeholders, started working on Homepage

--- app/src/main/java/com/amadeus/ting/RegLogin.kt
+++ app/src/main/java/com/amadeus/ting/RegLogin.kt
@@ -1,11 +1,27 @@
 package com.amadeus.ting
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class RegLogin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reg_login)
+
+        val googleloginButton = findViewById<Button>(R.id.google_login_button)
+        val signupButton = findViewById<TextView>(R.id.signup_button)
+        val loginButton = findViewById<Button>(R.id.login_button)
+        signupButton.setOnClickListener{
+            val Intent = Intent(this,SignUp::class.java)
+            startActivity(Intent)
+        }
+        loginButton.setOnClickListener{
+            val Intent = Intent(this,Login::class.java)
+            startActivity(Intent)
+        }
+
     }
 }
