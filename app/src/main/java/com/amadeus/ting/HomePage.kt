6789d578b2d28d1d897898f8142e1d71b551c59d
Dustin Jed Wesley Esponilla <dustinesponilla@gmail.com>
2023-03-08 23:26:16 +0800

    added buttons and some new activities on the app's homepage

--- app/src/main/java/com/amadeus/ting/HomePage.kt
+++ app/src/main/java/com/amadeus/ting/HomePage.kt
@@ -1,11 +1,47 @@
 package com.amadeus.ting
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.imageview.ShapeableImageView
 
 class HomePage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
+
+
+        onClick<ConstraintLayout>(R.id.linearLayout_HealthSection) {
+            val goToHealthWellness = Intent(this, HealthAndWellness::class.java)
+            startActivity(goToHealthWellness)
+        }
+
+        onClick<ConstraintLayout>(R.id.linearLayout_PlannerSection) {
+            val goToPlanner = Intent(this, Planner::class.java)
+            startActivity(goToPlanner)
+        }
+
+        onClick<ConstraintLayout>(R.id.linearLayout_FocusSection) {
+            val goToFocus = Intent(this, FocusMode::class.java)
+            startActivity(goToFocus)
+        }
+
+        onClick<ShapeableImageView>(R.id.user_image) {
+            val goToProgress = Intent(this, ProgressReport::class.java)
+            startActivity(goToProgress)
+        }
+
     }
+
+    private inline fun <reified T : View> Activity.onClick(id: Int, crossinline action: (T) -> Unit) {
+        findViewById<T>(id)?.setOnClickListener {
+            action(it as T)
+        }
+    }
+
+
+
 }
---
+++ app/src/main/java/com/amadeus/ting/ProgressReport.kt
@@ -0,0 +1,11 @@
+package com.amadeus.ting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProgressReport : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_progress_report)
+    }
+}
