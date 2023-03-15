0c2e2f3f9a66b055d90eb899967a5538b7418391
Dustin Jed Wesley Esponilla <dustinesponilla@gmail.com>
2023-03-15 18:06:22 +0800

    showCustomDialog fix with nestedPopupLayout

--- app/src/main/java/com/amadeus/ting/Planner.kt
+++ app/src/main/java/com/amadeus/ting/Planner.kt
@@ -14,6 +14,7 @@ import android.graphics.drawable.ColorDrawable
 import android.view.LayoutInflater
 import android.widget.Button
 import android.widget.PopupWindow
+import android.widget.TextView
 
 class Planner : AppCompatActivity() {
 
@@ -36,13 +37,9 @@ class Planner : AppCompatActivity() {
         // Sort -> Dust
         onClick<ShapeableImageView>(R.id.sort_button) {
             val labelAlert = MyAlertDialog()
-            labelAlert.showCustomDialog(this, R.layout.sort_popupwindow)
-
-            onClick<Button>(R.id.create_button) {
-                // code here
-            }
-
+            labelAlert.showCustomDialog(this, R.layout.sort_popupwindow, R.layout.popupwindow)
         }
+
     }
 
     private inline fun <reified T : View> Activity.onClick(id: Int, crossinline action: (T) -> Unit) {
@@ -54,23 +51,50 @@ class Planner : AppCompatActivity() {
 }
 
 class MyAlertDialog {
-    fun showCustomDialog(context: Context, popupLayout: Int) {
-        val builder = AlertDialog.Builder(context, R.style.MyDialogStyle)
+
+    fun showCustomDialog(context: Context, popupLayout: Int, nestedPopupLayout: Int = -1) {
         val inflater = LayoutInflater.from(context)
         val dialogLayout = inflater.inflate(popupLayout, null)
 
-        // Cancel Button
-        val cancelButton = dialogLayout.findViewById<Button>(R.id.cancel_button)
+        val builder = AlertDialog.Builder(context, R.style.MyDialogStyle)
         builder.setView(dialogLayout)
+        builder.setCancelable(false)
 
-        builder.setCancelable(false) // Set cancelable to false
+        val cancelButton = dialogLayout.findViewById<Button>(R.id.cancel_button)
         val dialog = builder.create()
-        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
 
         cancelButton.setOnClickListener {
             dialog.dismiss()
         }
 
+        // Nested Dialog: -1 if there is no need for nested dialog
+        var nestedDialog: AlertDialog? = null
+
+        if (nestedPopupLayout != -1) {
+            val showNestedDialogButton = dialogLayout.findViewById<Button>(R.id.text_alphabetical)
+
+            showNestedDialogButton.setOnClickListener {
+
+                val nestedDialogLayout = inflater.inflate(nestedPopupLayout, null)
+                val nestedBuilder = AlertDialog.Builder(context, R.style.MyDialogStyle)
+                nestedBuilder.setView(nestedDialogLayout)
+                builder.setCancelable(false)
+
+                nestedDialog = nestedBuilder.create()
+                nestedDialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+                nestedDialog?.show()
+
+                val cancelButtonNested = nestedDialogLayout.findViewById<Button>(R.id.cancel_button)
+                cancelButtonNested.setOnClickListener {
+                    nestedDialog?.dismiss()
+                }
+
+            }
+        }
+
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
         dialog.show()
     }
+
 }
+
