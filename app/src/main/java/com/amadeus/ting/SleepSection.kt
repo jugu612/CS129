e1283e8c9295f3a28fcd2136536651dc3bdb5561
jugu612 <jngudito@up.edu.ph>
2023-05-20 15:38:26 +0800

    Updated sleeping reminder feature

--- app/src/main/java/com/amadeus/ting/SleepSection.kt
+++ app/src/main/java/com/amadeus/ting/SleepSection.kt
@@ -1,9 +1,19 @@
 package com.amadeus.ting
 
+import android.app.AlertDialog
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.imageview.ShapeableImageView
 
 class SleepSection : AppCompatActivity() {
     private lateinit var sleepadapter: SleepAdapter
@@ -18,5 +28,39 @@ class SleepSection : AppCompatActivity() {
         val layoutManager = LinearLayoutManager(this)
         recyclerView.layoutManager = layoutManager
 
+        val backButton: ShapeableImageView = findViewById(R.id.back_button)
+        backButton.setOnClickListener {
+            val goToHomePage = Intent(this, HomePage::class.java)
+            startActivity(goToHomePage)
+        }
+
+        val editButton: ShapeableImageView = findViewById(R.id.edit_button)
+        editButton.setOnClickListener {
+            showCustomDialog(this, R.layout.edit_sleep)
+        }
+
+
+    }
+
+    fun showCustomDialog(context: Context, popupLayout: Int) {
+        val inflater = LayoutInflater.from(context)
+        val dialogLayout = inflater.inflate(popupLayout, null)
+
+        val builder = AlertDialog.Builder(context, R.style.MyDialogStyle)
+        builder.setView(dialogLayout)
+        builder.setCancelable(false)
+
+        val cancelButton = dialogLayout.findViewById<Button>(R.id.cancel_button)
+        val dialog = builder.create()
+        cancelButton.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        val saveButton = dialogLayout.findViewById<Button>(R.id.save_button)
+        saveButton.setOnClickListener {
+            dialog.dismiss() // Close the dialog
+        }
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.show()
     }
 }
